d50da14f2ea534b6db39f8199c32e6fdc426c3e6
Guilherme Vianna <mataveli91@gmail.com>
2025-08-21 21:26:47 -0300

    feat: add visualization of image links and some styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import axios from "axios";
 import TopBar from "./components/TopBar.vue";
-import { useFileDialog } from "@vueuse/core";
+import {
+  notNullish,
+  useAsyncState,
+  useClipboard,
+  useFileDialog,
+  useLocalStorage,
+  useWindowSize,
+} from "@vueuse/core";
 import { API_URL } from "../enviroments";
 import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
@@ -12,26 +19,49 @@ const { files, open, reset, onCancel, onChange } = useFileDialog({
 
 const images = ref([]);
 
+async function loadImages() {
+  const sessionId = localStorage.getItem("sessionId");
+  if (sessionId == null) useLocalStorage("sessionId", uuidv4());
+
+  const resultImages = await axios.get(
+    API_URL + `/image?sessionId=${sessionId}`
+  );
+  images.value = resultImages.data;
+}
+
+const { state, execute, isLoading } = useAsyncState(loadImages, null, {
+  immediate: false,
+});
+
 onChange(async (files) => {
   if (!files || files.length === 0) {
     return;
   }
+  const sessionId = localStorage.getItem("sessionId");
+  if (sessionId == null) {
+    console.error("SessionId not found");
+  }
 
-  let id = uuidv4();
   let formData = new FormData();
   formData.append("file", files?.[0]);
-  formData.append("sessionId", id);
+  formData.append("sessionId", sessionId);
 
   const result = await axios.post(API_URL + `/image`, formData);
 
   if (result.status == 200) {
-    debugger;
-    const resultImages = await axios.get(API_URL + `/image?sessionId=${id}`);
-    images.value = resultImages.data;
+    execute();
   }
 });
 
+execute();
+
 onCancel(() => {});
+
+const { copy } = useClipboard();
+function copyLink(url) {
+  copy(url);
+  alert("Link Copiado");
+}
 </script>
 
 <template>
@@ -44,13 +74,29 @@ onCancel(() => {});
     <button
       type="button"
       @click="open()"
-      class="mt-20 bg-green-900 p-5 px-10 rounded-sm text-white shadow-2xl"
+      class="mt-20 bg-green-900 p-5 px-10 rounded-sm text-white shadow-2xl cursor-pointer"
     >
-      Subir Imagem
+      Adicionar Imagem
     </button>
-    <ol>
-      <li>Teste</li>
-      <li v-for="x in images">{{ x.url }}</li>
+    <ol
+      class="flex flex-col min-w-4/12 bg-green-800 rounded-xl p-5 mt-12 text-white font-bold gap-12"
+    >
+      <div v-if="isLoading">Carregando Imagens...</div>
+      <li v-for="x in images">
+        <div class="flex justify-between items-center gap-5 sm:px-5">
+          <a class="max-w-5/12">
+            {{
+              x.url.slice(useWindowSize().width.value < 450 ? 75 : 62, Infinity)
+            }}
+          </a>
+          <button
+            class="p-2 bg-white rounded-sm text-green-800 cursor-pointer"
+            @click="copyLink(x.url)"
+          >
+            Copiar Link
+          </button>
+        </div>
+      </li>
     </ol>
   </div>
 </template>
