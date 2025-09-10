9f90ab2cc5128ccf5d41a5194cce0a3072fd0b7c
Guilherme Vianna <mataveli91@gmail.com>
2025-09-10 20:20:03 -0300

    style: ajustando botoes e titulos

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,12 +1,12 @@
 <template>
-  <div
-      class="flex justify-end items-center p-4 px-5 gap-7 bg-green-900 text-white"
-  >
-    <button class="flex justify-center items-center gap-2 bg-white p-2 px-5 text-black rounded-sm shadow-2xl">
-      <div class="pi pi-folder">
+  <div class="flex items-center p-7 px-5 gap-7 bg-green-900 text-white">
+    <div class="font-bold text-2xl">Image Upload</div>
 
-      </div>
+    <!-- <button
+      class="flex justify-center items-center gap-2 bg-white p-2 px-5 text-black rounded-sm shadow-2xl"
+    >
+      <div class="pi pi-folder"></div>
       Meus Arquivos
-    </button>
+    </button> -->
   </div>
 </template>
